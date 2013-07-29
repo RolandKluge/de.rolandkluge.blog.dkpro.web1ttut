b15e3351c412dd8bc920c0f9f6268bb42532bdf4
Roland Kluge <roland.kluge@gmx.de>
2013-07-29 18:11:31 +0200

    Initial commit of code

---
+++ src/main/java/de/rolandkluge/blog/dkpro/web1ttut/Web1TTut.java
@@ -0,0 +1,26 @@
+package de.rolandkluge.blog.dkpro.web1ttut;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.googlecode.jweb1t.JWeb1TSearcher;
+
+public class Web1TTut
+{
+    public static void main(final String[] args)
+    {
+        final String dkproHome = System.getenv("DKPRO_HOME");
+        try {
+            final JWeb1TSearcher web1TSearcher = new JWeb1TSearcher(new File(dkproHome, "web1t/ENGLISH"), 1, 3);
+            for (final String phrase : Arrays.asList("house", "like you", "What a wonderful"))
+            {
+                final long count = web1TSearcher.getFrequency(phrase);
+                System.out.println(String.format("Count for '%s': %s", phrase, count));
+            }
+        }
+        catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
